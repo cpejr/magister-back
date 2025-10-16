e2004f6541740a3df0460a4e85a1ec74f3432729
matheusmadeira <fumacammadeira@gmail.com>
2025-10-16 13:59:41 -0300

    fix: text

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 const dotenv = require ("dotenv")
 const app = require ("./App");
 
--- src/Controller/transcricaoController.js
+++ src/Controller/transcricaoController.js
@@ -1,16 +1,13 @@
 const { transcreverTexto } = require("../transcricao.js");
 
 function create(req, res) {
-  
-  try{
-    const { texto } = req.body;
-    const resultado = transcreverTexto(texto);
+  try {
+    const { text } = req.body;
+    const resultado = transcreverTexto(text);
     return res.json({ resultado });
-  } 
-  catch (error){
+  } catch (error) {
     return res.status(400).json({ erro: "Texto é obrigatório." });
   }
- 
 }
 
 module.exports = { create };
